6ca60e2a06eb4277af7322d220c0ce7cbca98c59
Federico Costa <fede.costa@live.com>
2020-10-21 03:12:59 -0300

    Added WebDriver configuration, method to make whole page load using JavaScript, and basic navigation steps specifically for the test

--- src/test/java/com/UIBaseTest.java
+++ src/test/java/com/UIBaseTest.java
@@ -1,10 +1,16 @@
 package com;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class UIBaseTest {
 
@@ -14,12 +20,32 @@ public class UIBaseTest {
     public void setupClass() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
     @AfterClass
     public void teardown() {
         if (driver != null) {
             driver.quit();
+        }
+    }
+
+    protected void scrollUntilFullPageIsLoaded() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        Object documentHeight = js.executeScript("return document.body.scrollHeight");
+
+        long y = 500;
+        Duration timeout = Duration.ofSeconds(20);
+
+        while (y <= (Long) documentHeight) {
+            ExpectedCondition<Boolean> pageLoadCondition =
+                    driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").
+                            equals("complete");
+            WebDriverWait wait = new WebDriverWait(driver, timeout);
+            js.executeScript("window.scrollTo(0, " + y + ");");
+            wait.until(pageLoadCondition);
+            y += 500;
         }
     }
 }
--- src/test/java/com/aliexpress/ProductSearchTest.java
+++ src/test/java/com/aliexpress/ProductSearchTest.java
@@ -1,6 +1,9 @@
 package com.aliexpress;
 
 import com.UIBaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ProductSearchTest extends UIBaseTest {
@@ -8,6 +11,17 @@ public class ProductSearchTest extends UIBaseTest {
     @Test
     public void TestProductSearch() {
         driver.get("http://aliexpress.com");
+        //TODO: Check for overlays
+        driver.findElement(By.id("search-key")).sendKeys("iphone\n");
+        //Need to scroll down first for whole page to be loaded
+        scrollUntilFullPageIsLoaded();
+        WebElement element = driver.findElement(By.className("next-pagination-list"))
+                            .findElement(By.xpath("button[contains(text(), '2')]"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element);
+        actions.perform();
+        element.click();
+        driver.findElements(By.cssSelector(".list-item")).get(1).click();
     }
 
 }
