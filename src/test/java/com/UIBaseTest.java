28ad546ba8a172dcf9914fe55376aa6b22f60aac
Federico Costa <fede.costa@live.com>
2020-10-22 00:59:06 -0300

    Completed test main actions, added way to close any pending wd instance after suite, bumped source to v8 in pom.xml

--- src/test/java/com/UIBaseTest.java
+++ src/test/java/com/UIBaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 
 import java.time.Duration;
@@ -26,6 +27,13 @@ public class UIBaseTest {
 
     @AfterClass
     public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @AfterSuite
+    public void quit() {
         if (driver != null) {
             driver.quit();
         }
--- src/test/java/com/aliexpress/ProductSearchTest.java
+++ src/test/java/com/aliexpress/ProductSearchTest.java
@@ -2,18 +2,31 @@ package com.aliexpress;
 
 import com.UIBaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
 
 public class ProductSearchTest extends UIBaseTest {
 
     @Test
     public void TestProductSearch() {
         driver.get("http://aliexpress.com");
-        //TODO: Check for overlays
         driver.findElement(By.id("search-key")).sendKeys("iphone\n");
-        //Need to scroll down first for whole page to be loaded
+
+        try {
+            driver.findElement(By.className("next-dialog-close")).click();
+        } catch (NoSuchElementException e) {
+            Reporter.log("No overlay was found");
+        }
+
         scrollUntilFullPageIsLoaded();
         WebElement element = driver.findElement(By.className("next-pagination-list"))
                             .findElement(By.xpath("button[contains(text(), '2')]"));
@@ -21,7 +34,20 @@ public class ProductSearchTest extends UIBaseTest {
         actions.moveToElement(element);
         actions.perform();
         element.click();
-        driver.findElements(By.cssSelector(".list-item")).get(1).click();
+        clickOnAd();
+        ArrayList<String> windowHandles =  new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windowHandles.get(windowHandles.size() - 1));
+        Reporter.log("Done");
+    }
+
+    private void clickOnAd() {
+        try {
+            driver.findElements(By.cssSelector(".list-item")).get(1).click();
+        } catch (StaleElementReferenceException e) {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.elementToBeClickable(By.className("logo-base")));
+            clickOnAd();
+        }
     }
 
 }
